ecff7c00dab383215b475f4b2874fc54ece02c6f
Harry Boyd <hboyd255@gmail.com>
2024-08-13 10:39:47 +0100

    Added test to read both registers at once

--- README.md
+++ README.md
@@ -7,3 +7,29 @@ the NES controller and the bumpers on the Project A.L.I.E.N. robot.
 
 Both shift registers are 4021 shift registers, which are 8-bit
 parallel-in/serial-out.
+
+## Notes
+
+### For the bumper button shift register
+
+The state of the 8 buttons around the robot are read in as a byte.The lsb is the
+value of the front button, then the ascending bits represent the subsequent
+buttons rotating clockwise.
+
+## Pull up vs pull down resistors
+
+The NES controller uses pull up resistors, while the bumper buttons use pull
+down resistors.
+
+This means that for the nes controller, a 1 represents a button not being
+pressed and a 0 represents a button being pressed. For the bumper buttons, a 0
+represents a button not being pressed and a 1 represents a button being pressed.
+
+I don't fully remember why I designed the bumpers using pull down resistors,
+pull up resistors would have been easier to implement. Maybe I was thinking that
+0 representing not pressed would be more intuitive. Or maybe I had an excess of
+red wire to make a 5V rail.
+
+This difference in pull up vs pull down resistors isn't a problem, but is
+something that must be accounted for in the code.
+
--- src/main.cpp
+++ src/main.cpp
@@ -25,20 +25,78 @@
 // Font used - ANSI Shadow
 
 #include <Arduino.h>
-#include <NESControllerInterface.h>
 
+#include "binary.h"
 #include "systemInfo.h"
 
-NESControllerInterface nes(NES_SHIFT_REG_DATA, COMMON_SHIFT_REG_LOAD,
-                           COMMON_SHIFT_REG_CLOCK);
+/**
+ * @brief Reads two bytes from two separate 4021 shift registers.
+ * Both shift registers are connected to the same clock and load pin, but have
+ * separate data pins.
+ *
+ * @param dataPin1 The data pin for the first shift register.
+ * @param dataPin2 The data pin for the second shift register.
+ * @param loadPin The load pin for both shift registers.
+ * @param clockPin The clock pin for both shift registers.
+ * @param byte1 The byte to store the data from the first shift register.
+ * @param byte2 The byte to store the data from the second shift register.
+ */
+void readTwoSeparateBytes(int dataPin1, int dataPin2, int loadPin, int clockPin,
+                          uint8_t *byte1, uint8_t *byte2) {
+    // Create a byte for storing the received data from the shift register.
+    uint8_t shiftRegister1Contents = 0;
+    uint8_t shiftRegister2Contents = 0;
+
+    // Pulse the load pin to load the button states into the shift
+    // register.
+    digitalWrite(loadPin, LOW);
+    delayMicroseconds(10);
+    digitalWrite(loadPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(loadPin, LOW);
+
+    // For each button,
+    for (uint8_t i = 0; i < 8; i++) {
+        // Shift the received data up by one.
+        shiftRegister1Contents <<= 1;
+        shiftRegister2Contents <<= 1;
+
+        // Load the available bit from the shift registers into the lsb of the
+        // received data byte.
+        shiftRegister1Contents |= digitalRead(dataPin1);
+        shiftRegister2Contents |= digitalRead(dataPin2);
+
+        // Pulse the clock pin to shift the data inside the shift registers.
+        digitalWrite(clockPin, LOW);
+        delayMicroseconds(10);
+        digitalWrite(clockPin, HIGH);
+        delayMicroseconds(10);
+    }
+    *byte1 = shiftRegister1Contents;
+    *byte2 = shiftRegister2Contents;
+}
 
 void setup() {
     // Initialize serial communication
     Serial.begin(SERIAL_BAUD_RATE);
+
+    pinMode(NES_SHIFT_REG_DATA, INPUT_PULLUP);
+    pinMode(BUMPER_SHIFT_REG_DATA, INPUT_PULLUP);
+    pinMode(COMMON_SHIFT_REG_LOAD, OUTPUT);
+    pinMode(COMMON_SHIFT_REG_CLOCK, OUTPUT);
 }
 
 void loop() {
-    // Test the NES controller
-    NESInput NESData = nes.getNESInput();
-    Serial.println(NESData);
+    byte bumper1, NESValue;
+
+    readTwoSeparateBytes(BUMPER_SHIFT_REG_DATA, NES_SHIFT_REG_DATA,
+                         COMMON_SHIFT_REG_LOAD, COMMON_SHIFT_REG_CLOCK,
+                         &bumper1, &NESValue);
+
+    Serial.println("Bumper Values:");
+    printByte(bumper1, ",");
+    Serial.println("NES Values:");
+    printByte(NESValue, ",");
+    Serial.println();
+    delay(100);
 }
